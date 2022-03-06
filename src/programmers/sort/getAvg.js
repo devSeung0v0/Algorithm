fa7598ee8714110f53364e14c8d11093d58ea661
seungyeon Lee <asfire0302@gmail.com>
2022-03-07 02:37:01 +0900

    🎨 코드 개선 - reduce 연산자 사용

--- src/programmers/sort/getAvg.js
+++ src/programmers/sort/getAvg.js
@@ -1,8 +1,16 @@
-function solution(arr) {
-    var answer = 0;
-    for(var i = 0; i < arr.length; i ++){
-        answer += arr[i];
-    }
+//이전 풀이
+// function solution(arr) {
+//     var answer = 0;
+//     for(var i = 0; i < arr.length; i ++){
+//         answer += arr[i];
+//     }
 
-    return answer / arr.length;
+//     return answer / arr.length;
+// }
+
+//새로운 풀이 - reduce 연산자 사용
+function solution(arr) {
+    const answer = arr.reduce((prev,curr) => prev + curr) / arr.length
+    
+    return answer;
 }
