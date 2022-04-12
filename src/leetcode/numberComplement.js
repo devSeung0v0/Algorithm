69e2c0badac3b37f871c9bb03153d09eadeb48b9
seungyeon Lee <asfire0302@gmail.com>
2022-04-12 23:44:53 +0900

    :sparkle: 476. Number Complement

---
+++ src/leetcode/numberComplement.js
@@ -0,0 +1,8 @@
+const findComplement = (num)=> {
+  const str = num.toString(2)
+  let result = '';
+  for (let i of str) {
+    result += i === '1'?  '0': '1';
+  }
+  return parseInt(result, 2);
+};
