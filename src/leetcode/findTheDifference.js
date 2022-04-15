94d26f4e81e7a41ca4754e9dbdc2ebdfa92025bf
seungyeon Lee <asfire0302@gmail.com>
2022-04-15 21:48:01 +0900

    :bulb: 코드 설명 주석 추가

--- src/leetcode/findTheDifference.js
+++ src/leetcode/findTheDifference.js
@@ -1,7 +1,7 @@
 const findTheDifference= (s, t)=>{
-  const sArr = s.split('').sort()
+  const sArr = s.split('').sort()//문자열을 배열로 바꿔서 정렬시키기
   const tArr = t.split('').sort()
-  for(i=0; i<tArr.length; i++ ){
+  for(i=0; i<tArr.length; i++ ){//정렬된 배열의 값을 하나하나 비교해서 같지 않은 값을 반환
       if(sArr[i] !== tArr[i]){
           return tArr[i]
       }
