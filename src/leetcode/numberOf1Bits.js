488f6e0a6ec5e3bec5ef2285fd0fc7f1c7e41f5a
seungyeon Lee <asfire0302@gmail.com>
2022-04-17 22:25:21 +0900

    :sparkles: 191. Number of 1 Bits

---
+++ src/leetcode/numberOf1Bits.js
@@ -0,0 +1,9 @@
+//question
+//Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+//https://leetcode.com/problems/number-of-1-bits/
+
+const hammingWeight = (n)=>{
+  const count = n.toString(2).split(1).length -1
+  return count
+
+};
